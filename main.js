6c60d86c86c8e9f60cc4e684ecca2c03e2503c15
Shreyas <72331003+Shreyas94@users.noreply.github.com>
2021-06-08 11:24:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function badm_rack(){
+    location.replace("badm_rack.html")
+}
+function mouse(){
+    location.replace("mouse.html");
+}
+
+function pb(){
+    location.replace("pb.html");
+}
+
+function lm(){
+    location.replace("lm.html");
+}
